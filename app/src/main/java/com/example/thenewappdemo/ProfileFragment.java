f2a2c8175420886e21b204ad556e0c4d001a8f48
chabuuuu <haphuthinh332004@gmail.com>
2023-03-14 21:32:38 +0700

    thêm chức năng đổi tên, thêm nút đổi tên và đổi ngành học

--- app/src/main/java/com/example/thenewappdemo/ProfileFragment.java
+++ app/src/main/java/com/example/thenewappdemo/ProfileFragment.java
@@ -1,16 +1,26 @@
 package com.example.thenewappdemo;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +30,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,7 +49,8 @@ public class ProfileFragment extends Fragment {
 
     ImageView avatarIv;
     TextView nameTv, emailTv, phoneTv;
-
+    FloatingActionButton fab;
+    ProgressDialog pd;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -94,12 +107,17 @@ public class ProfileFragment extends Fragment {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Users");
 
+
         //init view
 
         avatarIv = view.findViewById(R.id.avatarIv);
         nameTv = view.findViewById(R.id.nameTv);
         emailTv = view.findViewById(R.id.emailTv);
         phoneTv = view.findViewById(R.id.phoneTv);
+
+        fab = view.findViewById(R.id.fab);
+        pd = new ProgressDialog(getActivity());
+
 
 
         //tìm ra user theo email để lấy thông tin
@@ -145,11 +163,133 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        //Nút nổi đc nhất thì:
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEditProfileDialog();
+
+
+            }
+        });
+
 
 
 
 
 
         return view;
+    }
+
+    private void showEditProfileDialog() {
+        String options[] = {"Đổi tên", "Đổi ngành học của bạn"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Hãy chọn đi");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0){
+                    //edit name
+                    pd.setMessage("Đang đổi tên tài khoản");
+
+                    //gọi hàm edit tên
+                    showNamePhoneUpdateDialog("name");
+
+
+                }
+                else if (which == 1){
+                    //edit nganh hoc
+                    pd.setMessage("Đang đổi ngành học của bạn");
+
+                    // gọi hàm đổi ngành
+//                    showNamePhoneUpdateDialog("phone");
+
+
+
+                }
+
+            }
+        });
+        builder.create().show();
+
+
+    }
+
+    // Tạo khung nhập dữ liệu cần chỉnh sửa
+
+    private void showNamePhoneUpdateDialog(String key) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Update "+key);
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(10, 10, 10, 10);
+        EditText editText = new EditText(getActivity());
+        editText.setHint("Nhập " + key);
+        linearLayout.addView(editText);
+
+        builder.setView(linearLayout);
+
+        //thêm nút cập nhật
+
+        builder.setPositiveButton("Câp nhật", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // input text
+
+                String value = editText.getText().toString().trim();
+                //kiểm tra xem  người dùng có nhập gì hay không
+
+                if (!TextUtils.isEmpty(value)){
+                    pd.show();
+
+                    //Đưa phần tử nào đó vào trong database
+                    //ỨNG DỤNG LẠI CODE NÀY ĐỂ LÀM TÍNH NĂNG CHỌN MENU NGÀNH HỌC
+
+                    HashMap<String,Object> result = new HashMap<>();
+                    //đưa key và giả trị của nó vào hash map (vd: key là name, value là giá trị của nó)
+
+                    result.put(key, value);
+
+                    //code đưa giá trị vào databse
+                    databaseReference.child(user.getUid()).updateChildren(result).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            pd.dismiss();
+                            Toast.makeText(getActivity(), "Đã cập nhật tên bạn", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(getActivity(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+
+                }
+
+            }
+        });
+
+        //thêm nút cancel
+
+        builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+
+            }
+
+        });
+        builder.create().show();
+
+
+
     }
 }
